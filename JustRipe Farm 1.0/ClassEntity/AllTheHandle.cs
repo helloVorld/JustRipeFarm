118e5ec17a65c1b9efd94891546479629f19f0af
Adrian Tang <siong_8624@hotmail.com>
2018-11-29 22:38:15 +0800

    Fix errors

--- JustRipe Farm 1.0/ClassEntity/AllTheHandle.cs
+++ JustRipe Farm 1.0/ClassEntity/AllTheHandle.cs
@@ -7,104 +7,104 @@ using MySql.Data.MySqlClient;
 
 namespace JustRipeFarm.ClassEntity
 {
-    class AllTheHandle
-    {
-    }
+//    class AllTheHandle
+//    {
+//    }
 
-    public class BoxHandler
-    {
-        public int addNewOrder(MySqlConnection conn, Crop crop)
-        {
-            string sql = "INSERT INTO crops (name, quantity, growth_stage) "
-                        + " Values ('" + crop.Name + "', " + crop.Quantity + ", '" + crop.GrowthStage + "')";
+//    public class BoxHandler
+//    {
+//        public int addNewOrder(MySqlConnection conn, Crop crop)
+//        {
+//            string sql = "INSERT INTO crops (name, quantity, growth_stage) "
+//                        + " Values ('" + crop.Name + "', " + crop.Quantity + ", '" + crop.GrowthStage + "')";
 
-            MySqlCommand sqlComm = new MySqlCommand(sql, conn);
-            return sqlComm.ExecuteNonQuery();
-        }
-    }
+//            MySqlCommand sqlComm = new MySqlCommand(sql, conn);
+//            return sqlComm.ExecuteNonQuery();
+//        }
+//    }
 
-    public class OrderHandler
-    {
-        public int addNewOrder(MySqlConnection conn, Crop crop)
-        {
-            string sql = "INSERT INTO crops (name, quantity, growth_stage) "
-                        + " Values ('" + crop.Name + "', " + crop.Quantity + ", '" + crop.GrowthStage + "')";
+//    public class OrderHandler
+//    {
+//        public int addNewOrder(MySqlConnection conn, Crop crop)
+//        {
+//            string sql = "INSERT INTO crops (name, quantity, growth_stage) "
+//                        + " Values ('" + crop.Name + "', " + crop.Quantity + ", '" + crop.GrowthStage + "')";
 
-            MySqlCommand sqlComm = new MySqlCommand(sql, conn);
-            return sqlComm.ExecuteNonQuery();
-        }
-    }
-    public class ProductHandler
-    {
-        public int addNewProduct(MySqlConnection conn, Crop crop)
-        {
-            string sql = "INSERT INTO crops (name, quantity, growth_stage) "
-                        + " Values ('" + crop.Name + "', " + crop.Quantity + ", '" + crop.GrowthStage + "')";
+//            MySqlCommand sqlComm = new MySqlCommand(sql, conn);
+//            return sqlComm.ExecuteNonQuery();
+//        }
+//    }
+//    public class ProductHandler
+//    {
+//        public int addNewProduct(MySqlConnection conn, Crop crop)
+//        {
+//            string sql = "INSERT INTO crops (name, quantity, growth_stage) "
+//                        + " Values ('" + crop.Name + "', " + crop.Quantity + ", '" + crop.GrowthStage + "')";
 
-            MySqlCommand sqlComm = new MySqlCommand(sql, conn);
-            return sqlComm.ExecuteNonQuery();
-        }
-    }
+//            MySqlCommand sqlComm = new MySqlCommand(sql, conn);
+//            return sqlComm.ExecuteNonQuery();
+//        }
+//    }
 
     
 
-    public class FarmHandler
-    {
-        public int addNewFarm(MySqlConnection conn, Crop crop)
-        {
-            string sql = "INSERT INTO crops (name, quantity, growth_stage) "
-                        + " Values ('" + crop.Name + "', " + crop.Quantity + ", '" + crop.GrowthStage + "')";
+//    public class FarmHandler
+//    {
+//        public int addNewFarm(MySqlConnection conn, Crop crop)
+//        {
+//            string sql = "INSERT INTO crops (name, quantity, growth_stage) "
+//                        + " Values ('" + crop.Name + "', " + crop.Quantity + ", '" + crop.GrowthStage + "')";
 
-            MySqlCommand sqlComm = new MySqlCommand(sql, conn);
-            return sqlComm.ExecuteNonQuery();
-        }
-    }
+//            MySqlCommand sqlComm = new MySqlCommand(sql, conn);
+//            return sqlComm.ExecuteNonQuery();
+//        }
+//    }
 
-    public class EmployeeHandler
-    {
-        public int addNewEmployee(MySqlConnection conn, Crop crop)
-        {
-            string sql = "INSERT INTO crops (name, quantity, growth_stage) "
-                        + " Values ('" + crop.Name + "', " + crop.Quantity + ", '" + crop.GrowthStage + "')";
+//    public class EmployeeHandler
+//    {
+//        public int addNewEmployee(MySqlConnection conn, Crop crop)
+//        {
+//            string sql = "INSERT INTO crops (name, quantity, growth_stage) "
+//                        + " Values ('" + crop.Name + "', " + crop.Quantity + ", '" + crop.GrowthStage + "')";
 
-            MySqlCommand sqlComm = new MySqlCommand(sql, conn);
-            return sqlComm.ExecuteNonQuery();
-        }
-    }
+//            MySqlCommand sqlComm = new MySqlCommand(sql, conn);
+//            return sqlComm.ExecuteNonQuery();
+//        }
+//    }
 
-    public class CustomerHandler
-    {
-        public int addNewCustomer(MySqlConnection conn, Crop crop)
-        {
-            string sql = "INSERT INTO crops (name, quantity, growth_stage) "
-                        + " Values ('" + crop.Name + "', " + crop.Quantity + ", '" + crop.GrowthStage + "')";
+//    public class CustomerHandler
+//    {
+//        public int addNewCustomer(MySqlConnection conn, Crop crop)
+//        {
+//            string sql = "INSERT INTO crops (name, quantity, growth_stage) "
+//                        + " Values ('" + crop.Name + "', " + crop.Quantity + ", '" + crop.GrowthStage + "')";
 
-            MySqlCommand sqlComm = new MySqlCommand(sql, conn);
-            return sqlComm.ExecuteNonQuery();
-        }
-    }
+//            MySqlCommand sqlComm = new MySqlCommand(sql, conn);
+//            return sqlComm.ExecuteNonQuery();
+//        }
+//    }
 
-    public class StoreHandler
-    {
-        public int addNewStore(MySqlConnection conn, Crop crop)
-        {
-            string sql = "INSERT INTO crops (name, quantity, growth_stage) "
-                        + " Values ('" + crop.Name + "', " + crop.Quantity + ", '" + crop.GrowthStage + "')";
+//    public class StoreHandler
+//    {
+//        public int addNewStore(MySqlConnection conn, Crop crop)
+//        {
+//            string sql = "INSERT INTO crops (name, quantity, growth_stage) "
+//                        + " Values ('" + crop.Name + "', " + crop.Quantity + ", '" + crop.GrowthStage + "')";
 
-            MySqlCommand sqlComm = new MySqlCommand(sql, conn);
-            return sqlComm.ExecuteNonQuery();
-        }
-    }
+//            MySqlCommand sqlComm = new MySqlCommand(sql, conn);
+//            return sqlComm.ExecuteNonQuery();
+//        }
+//    }
 
-    public class InventoryHandler
-    {
-        public int addNewInventory(MySqlConnection conn, Crop crop)
-        {
-            string sql = "INSERT INTO crops (name, quantity, growth_stage) "
-                        + " Values ('" + crop.Name + "', " + crop.Quantity + ", '" + crop.GrowthStage + "')";
+//    public class InventoryHandler
+//    {
+//        public int addNewInventory(MySqlConnection conn, Crop crop)
+//        {
+//            string sql = "INSERT INTO crops (name, quantity, growth_stage) "
+//                        + " Values ('" + crop.Name + "', " + crop.Quantity + ", '" + crop.GrowthStage + "')";
 
-            MySqlCommand sqlComm = new MySqlCommand(sql, conn);
-            return sqlComm.ExecuteNonQuery();
-        }
-    }
+//            MySqlCommand sqlComm = new MySqlCommand(sql, conn);
+//            return sqlComm.ExecuteNonQuery();
+//        }
+//    }
 }
--- JustRipe Farm 1.0/ClassEntity/CropHandler.cs
+++ JustRipe Farm 1.0/ClassEntity/CropHandler.cs
@@ -9,15 +9,15 @@ namespace JustRipeFarm
 {
     public class CropHandler
     {
-        public int addNewCrop(MySqlConnection conn, Crop crop)
-        {
-            string sql = "INSERT INTO crops (name, quantity, growth_stage) "
-                        + " Values ('" + crop.Name + "', " + crop.Quantity + ", '" + crop.GrowthStage + "')";
+        //public int addNewCrop(MySqlConnection conn, Crop crop)
+        //{
+        //    string sql = "INSERT INTO crops (name, quantity, growth_stage) "
+        //                + " Values ('" + crop.Name + "', " + crop.Quantity + ", '" + crop.GrowthStage + "')";
 
-            MySqlCommand sqlComm = new MySqlCommand(sql, conn);
-            return sqlComm.ExecuteNonQuery();
-            
-            //I deleted something
-        }
+        //    MySqlCommand sqlComm = new MySqlCommand(sql, conn);
+        //    return sqlComm.ExecuteNonQuery();
+
+        //    //I deleted something
+        //}
     }
 }
