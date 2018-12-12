254cc35111134831955d53bbe96d6688d09d9ec9
Adrian Tang <siong_8624@hotmail.com>
2018-12-12 16:04:22 +0800

    Login work in progress

--- JustRipe Farm 1.0/AdminDashboard.Designer.cs
+++ JustRipe Farm 1.0/AdminDashboard.Designer.cs
@@ -58,11 +58,39 @@
             this.btnNewItem = new System.Windows.Forms.Button();
             this.dgvDbTable = new System.Windows.Forms.DataGridView();
             this.lblPanelTitle = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
             this.panelHome.SuspendLayout();
             this.panelOperation.SuspendLayout();
             this.panelInventory.SuspendLayout();
             this.panelCommon.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvDbTable)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // logoutBtn
@@ -95,7 +123,6 @@
             this.menuFLP.Name = "menuFLP";
             this.menuFLP.Size = new System.Drawing.Size(160, 468);
             this.menuFLP.TabIndex = 11;
-            
             // 
             // userLbl
             // 
@@ -150,8 +177,10 @@
             // panelHome
             // 
             this.panelHome.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelHome.Controls.Add(this.button2);
+            this.panelHome.Controls.Add(this.button1);
             this.panelHome.Controls.Add(this.label4);
-            this.panelHome.Location = new System.Drawing.Point(181, 84);
+            this.panelHome.Location = new System.Drawing.Point(181, 50);
             this.panelHome.Margin = new System.Windows.Forms.Padding(4);
             this.panelHome.Name = "panelHome";
             this.panelHome.Size = new System.Drawing.Size(866, 492);
@@ -274,7 +303,6 @@
             // tbId
             // 
             this.tbId.Enabled = false;
-            this.tbId.Location = new System.Drawing.Point(69, 357);
             this.tbId.Location = new System.Drawing.Point(92, 439);
             this.tbId.Margin = new System.Windows.Forms.Padding(4);
             this.tbId.Name = "tbId";
@@ -383,14 +411,252 @@
             this.lblPanelTitle.TabIndex = 0;
             this.lblPanelTitle.Text = "lblPanelTitle";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(508, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 47);
+            this.button1.TabIndex = 20;
+            this.button1.Text = "Edit Profile";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(637, 42);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(114, 47);
+            this.button2.TabIndex = 21;
+            this.button2.Text = "Change Password";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label10);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.textBox5);
+            this.panel1.Controls.Add(this.textBox4);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Location = new System.Drawing.Point(857, 42);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(355, 453);
+            this.panel1.TabIndex = 22;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(41, 79);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 17);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "First Name";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(41, 117);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(76, 17);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Last Name";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(7, 158);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(110, 17);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "Date of Birthday";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(68, 196);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(49, 17);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "mobile";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(75, 230);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(42, 17);
+            this.label9.TabIndex = 4;
+            this.label9.Text = "Email";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(141, 79);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(141, 117);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(141, 158);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(141, 196);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 22;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(141, 230);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 22);
+            this.textBox5.TabIndex = 23;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(71, 287);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(89, 45);
+            this.button3.TabIndex = 24;
+            this.button3.Text = "Save Change";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(178, 287);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(89, 45);
+            this.button4.TabIndex = 25;
+            this.button4.Text = "Cancel";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(102, 32);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(76, 17);
+            this.label10.TabIndex = 26;
+            this.label10.Text = "Edit Profile";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.textBox8);
+            this.panel2.Controls.Add(this.textBox7);
+            this.panel2.Controls.Add(this.textBox6);
+            this.panel2.Controls.Add(this.button6);
+            this.panel2.Controls.Add(this.button5);
+            this.panel2.Controls.Add(this.label15);
+            this.panel2.Controls.Add(this.label14);
+            this.panel2.Controls.Add(this.label13);
+            this.panel2.Controls.Add(this.label12);
+            this.panel2.Location = new System.Drawing.Point(1218, 43);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(331, 452);
+            this.panel2.TabIndex = 22;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(110, 41);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(122, 17);
+            this.label12.TabIndex = 27;
+            this.label12.Text = "Change Password";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(61, 80);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(73, 17);
+            this.label13.TabIndex = 28;
+            this.label13.Text = "Username";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(65, 121);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(69, 17);
+            this.label14.TabIndex = 29;
+            this.label14.Text = "Password";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(13, 160);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(121, 17);
+            this.label15.TabIndex = 30;
+            this.label15.Text = "Confirm Password";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(55, 229);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(89, 45);
+            this.button5.TabIndex = 31;
+            this.button5.Text = "Save Change";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(176, 229);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(89, 45);
+            this.button6.TabIndex = 32;
+            this.button6.Text = "Cancel";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(151, 80);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 22);
+            this.textBox6.TabIndex = 33;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(151, 121);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 22);
+            this.textBox7.TabIndex = 34;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(151, 160);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 22);
+            this.textBox8.TabIndex = 35;
+            // 
             // AdminDashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1827, 922);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panelHome);
             this.Controls.Add(this.panelCommon);
             this.Controls.Add(this.panelInventory);
-            this.Controls.Add(this.panelHome);
             this.Controls.Add(this.panelOperation);
             this.Controls.Add(this.formHeightLbl);
             this.Controls.Add(this.formWidthLbl);
@@ -413,6 +679,10 @@
             this.panelCommon.ResumeLayout(false);
             this.panelCommon.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvDbTable)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -450,5 +720,31 @@
         private System.Windows.Forms.Button btnNewItem;
         private System.Windows.Forms.DataGridView dgvDbTable;
         private System.Windows.Forms.Label lblPanelTitle;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label12;
     }
 }
---
+++ JustRipe Farm 1.0/Login.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.Data;
+using System.Drawing;
+using System.ComponentModel;
+using MySql.Data.MySqlClient;
+
+namespace JustRipeFarm.ClassEntity
+{
+    public class Login
+    {
+
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+      
+
+        public Login(string user, string pass)
+        {
+            this.Username = user;
+            this.Password = pass;  
+        }
+        
+        private void ClearTexts(string user, string pass)
+        {
+            user = String.Empty;
+            pass = String.Empty;
+        }
+
+        internal bool IsLoggedIn(string user, string pass)
+        {
+            if (string.IsNullOrEmpty(user))
+            {
+                MessageBox.Show("Enter the user name!");
+                return false;
+
+            }
+            else
+            {
+                if (Username != user)
+                {
+                    MessageBox.Show("User name is incorrect!");
+                    ClearTexts(user, pass);
+                    return false;
+                }
+                else
+                {
+                    if (string.IsNullOrEmpty(pass))
+                    {
+                        MessageBox.Show("Enter the passowrd!");
+                        return false;
+                    }
+                    else if (Password != pass)
+                    {
+                        MessageBox.Show("Password is incorrect");
+                        return false;
+                    }
+                    else
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        public int checkUserLogin()
+        {
+            MySqlDataReader rdr = null;
+            try
+            {
+                string sql = "SELECT * FROM employee WHERE username = '" + Username + "' AND password = '" + Password + "'";
+                MySqlCommand cmd = new MySqlCommand(sql, MysqlDbc.Instance.getConn());
+                rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+
+                    //cr.Id = rdr.GetInt32("id");
+                    //cr.Name = rdr.GetString("name");
+                    //cr.Type = rdr.GetString("type");
+                    //cr.Quantity_plot = rdr.GetInt32("quantity_plot");
+                    //cr.Remark = rdr.GetString("remark");
+                    Console.WriteLine("login => " );
+                    //Console.WriteLine("login => " + rdr.GetInt32("admin"));
+                    //cropLists.Add(cr);
+                    return rdr.GetInt32("admin");
+                }
+
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine("Error: {0}", ex.ToString());
+
+            }
+            finally
+            {
+                if (rdr != null)
+                {
+                    rdr.Close();
+                }
+
+            }
+            return 99;
+        }
+
+    }
+}
