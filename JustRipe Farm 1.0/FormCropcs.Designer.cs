574124ef48c4d24878e2a3691ce230b90e940daf
Gurmeet Singh <gurmeetsingh702@gmail.com>
2018-12-15 14:07:46 +0800

    testing data

--- JustRipe Farm 1.0/FormCropcs.Designer.cs
+++ JustRipe Farm 1.0/FormCropcs.Designer.cs
@@ -128,7 +128,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(387, 450);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
